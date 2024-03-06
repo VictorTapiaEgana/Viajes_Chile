80090ebba7b91b0fd1187cfce587ccccfd739f26
Victor <victortapiaegana@gmail.com>
2024-03-06 14:37:26 -0400

    logica al boton enviar de section contacto

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,16 +1,26 @@
 // BOOTSTRAP TOOLTIP
  
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
- const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
  //LLAMADO AL MODAL
 
- document.getElementById('botonenviar').addEventListener('click', function() {
+document.getElementById('botonenviar').addEventListener('click', function() {
     var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
 
-    $('modal-body').select(function () { 
-        
-    });
+    var nombreContacto = $('#nombre-contacto').val();    
+    var mensajeContacto = $('#mensajeContacto').val(); 
+    
+    $('.modal-body').html('<h5>' + nombreContacto + ': </h2>' +
+                          '<h6>Su Mensaje: <br>' +
+                          '<i>&ensp; &ensp; &ensp; &ensp; &ensp;"' +  mensajeContacto + '"</i>'+
+                          '<br> a sido enviado correctamente</h6>'
+                        );
     
     myModal.show();
-  });
+
+    $('#nombre-contacto').val('');
+    $('#asuntoContacto').val ('Seleccione una opcion');
+    $('#mensajeContacto').val ('');
+
+});
