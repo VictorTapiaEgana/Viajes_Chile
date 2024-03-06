1e4fc7656671aec3c1b5dcbace4e06527d8421f7
Victor <victortapiaegana@gmail.com>
2024-03-06 12:42:01 -0400

    se agrego el Select y modal

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,3 +1,16 @@
 // BOOTSTRAP TOOLTIP
- const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+ 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+ //LLAMADO AL MODAL
+
+ document.getElementById('botonenviar').addEventListener('click', function() {
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+
+    $('modal-body').select(function () { 
+        
+    });
+    
+    myModal.show();
+  });
